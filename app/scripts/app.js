caca1541f0f1f2888757278f8a958db0156ce6f5
mranosa <mark.ranosa@gmail.com>
2013-03-27 02:01:00 +0800

    Hello Trankeelo

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var trankeeloApp = angular.module('trankeeloApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
