c8b4005002e5c2efcaf4b85caaad78e9a4b9f84b
mranosa <mark.ranosa@gmail.com>
2013-03-28 05:13:44 +0800

    Added in initial auth service implementation

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,15 +1,17 @@
 'use strict';
 
-trankeeloApp.controller('MainCtrl', function($scope) {
-  $scope.awesomeThings = [
-    'HTML5 Boilerplate',
-    'AngularJS',
-    'Testacular'
-  ];
+trankeeloApp.controller('MainCtrl', function($scope, AuthService) {
+
+  $scope.signIn = function(){
+    AuthService.login();
+  };
 
   $('.hero-page').cycle({ 
-	    fx:     'scrollDown', 
-	    easing: 'easeOutBounce', 
-	    delay:  -2000 
-	});
+    fx:     'scrollDown', 
+    easing: 'easeOutBounce', 
+    delay:  -2000 
+  });
+
+  AuthService.init();
+
 });
--- app/scripts/services/AuthService.js
+++ app/scripts/services/AuthService.js
@@ -1,15 +1,27 @@
 'use strict';
 
 trankeeloApp.factory('AuthService', function() {
-  // Service logic
-  // ...
+	var USERS_URL = 'https://trankeelo.firebaseIO.com/users';
+	var totalUsersRef = new Firebase('https://trankeelo.firebaseIO.com/metrics/total_users');
+	var trankeeloRef = new Firebase('https://trankeelo.firebaseIO.com');
+	var authClient = undefined;
 
-  var meaningOfLife = 42;
+	var AuthService = function() {
+		
+	};
 
-  // Public API here
-  return {
-    someMethod: function() {
-      return meaningOfLife;
-    }
-  };
+	AuthService.prototype = {
+		init: function(){
+			authClient = new FirebaseAuthClient(trankeeloRef, function(error, user) {
+				if (!error) {
+					console.log(user);
+				}
+			});
+		},
+		login: function(){
+			authClient.login('facebook');
+		}
+	}
+
+	return new AuthService();
 });
