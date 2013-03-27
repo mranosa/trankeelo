4007c7ddc71d551dfac0a2a886a0e593f7797fe6
mranosa <mark.ranosa@gmail.com>
2013-03-28 04:39:01 +0800

    Initial authentication service implementation

---
+++ app/scripts/services/AuthService.js
@@ -0,0 +1,15 @@
+'use strict';
+
+trankeeloApp.factory('AuthService', function() {
+  // Service logic
+  // ...
+
+  var meaningOfLife = 42;
+
+  // Public API here
+  return {
+    someMethod: function() {
+      return meaningOfLife;
+    }
+  };
+});
